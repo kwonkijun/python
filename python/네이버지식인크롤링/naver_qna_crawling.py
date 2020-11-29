c3be60dee682a5b127ef380853c58920103302b1
kwonkijun <kkj6369@naver.com>
2020-11-29 13:33:15 +0900

    a

--- python/네이버지식인크롤링/naver_qna_crawling.py
+++ python/네이버지식인크롤링/naver_qna_crawling.py
@@ -1,14 +1,13 @@
 from urllib import request
 from urllib import parse
 from bs4 import BeautifulSoup
+import pyautogui
 import csv
 
-# 기본적으로 2가지 모듈이 필요하다. 
 
-# https://kin.naver.com/search/list.nhn?query=%ED%8C%8C%EC%9D%B4%EC%8D%AC
+keyword = pyautogui.prompt(text='검색어를 입력하세요', title='Message', default='입력하세요')
+lastPage = pyautogui.prompt(text='몇 페이지까지 검색할까요?', title='Message', default='입력하세요')
 
-keyword = input('검색어를 입력하세요')
-lastPage = input('몇 페이지까지 검색?')
 page = 1
 searchList = []
 
@@ -22,21 +21,8 @@ for i in range(1,int(lastPage)+1):
 
     print(f"------------------------------{i}번째 페이지----------------------------")
     for idx, li in enumerate(lists, 1):
-        temp = []
         title = li.select_one('dl > dt > a').get_text()
         date = li.select_one('dl > dd.txt_inline').get_text()
         content = li.select_one('dl > dd:nth-child(3)').get_text()
-        temp.append(title)
-        temp.append(date)
-        temp.append(content)
-        searchList.append(temp)
-
-f = open(f'{keyword}.csv', 'w', newline='', encoding='euc-kr')
-writer = csv.writer(f)
-for i in searchList:
-    writer.writerow(i)
-
-f.close()
-# enumerate '열거하다' 순서가 잇는 자료형(tuple, list, string)을 첫번째 인자로 받아서 각각의 index 값과 value 값들을 enumerate 객체로 리턴한다. 
-
+        print(f'{date}\n{title}\n{content}')
 
